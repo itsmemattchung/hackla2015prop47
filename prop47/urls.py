ccdfdc3079ebd7e0d8c338ae19de0a8f5e48a1c9
matthewchung <itsmemattchung@gmail.com>
2015-06-06 15:35:55 -0700

    Added resources page

--- prop47/urls.py
+++ prop47/urls.py
@@ -21,8 +21,9 @@ import views
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^faq/', TemplateView.as_view(template_name="faq.html")),
-    url(r'^resources/', TemplateView.as_view(template_name="about.html")),
+    url(r'^about/', TemplateView.as_view(template_name="about.html")),
     url(r'^legal/', TemplateView.as_view(template_name="legal.html")),
     url(r'^step/(?P<step>[\w-]+)$', views.get_step_view, name="legal-step"),
+    url(r'^resources/', TemplateView.as_view(template_name="resources.html")),
     url(r'^', TemplateView.as_view(template_name="index.html")),
 ]
