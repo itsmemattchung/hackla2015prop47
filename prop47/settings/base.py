2697d4249bf0f4b80486f656d3d375983a5e4add
Matt Davis <matt@scarecrowvfx.com>
2015-06-06 13:41:37 -0700

    adding bootstrap js and css files, and homepage html

--- prop47/settings/base.py
+++ prop47/settings/base.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'prop47.site',
 )
 
 MIDDLEWARE_CLASSES = (
--- prop47/urls.py
+++ prop47/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^', TemplateView.as_view(template_name="index.html"))
 ]
