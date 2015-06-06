568b58ea51a4353b813a955a010a7ca4da7704e0
Matt Davis <matt@scarecrowvfx.com>
2015-06-06 14:26:22 -0700

    added static pages, like legal faq and about.  basic bootstrap styling

--- prop47/urls.py
+++ prop47/urls.py
@@ -19,5 +19,8 @@ from django.views.generic.base import TemplateView
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^', TemplateView.as_view(template_name="index.html"))
+    url(r'^faq/', TemplateView.as_view(template_name="faq.html")),
+    url(r'^about/', TemplateView.as_view(template_name="about.html")),
+    url(r'^legal/', TemplateView.as_view(template_name="legal.html")),
+    url(r'^', TemplateView.as_view(template_name="index.html")),
 ]
