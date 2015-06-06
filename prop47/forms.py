deb8dec67087321ec42f7c1e70d60e6fb92b32cd
Matt Davis <matt@scarecrowvfx.com>
2015-06-06 15:27:46 -0700

    new views, updated templates with copy

--- prop47/forms.py
+++ prop47/forms.py
@@ -1,22 +1,33 @@
 from django import forms
 
 
+PROP_47_CRIMES = (
+    ("HD", "Haberdashery"),
+    ("ST", "Sartorialisation"),
+    ("PH", "Poor Hygene"),
+    )
+
+PRIOR_CRIMES = (
+    ("BL", "Blarney"),
+    ("SK", "Skullduggery"),
+    ("TF", "Tomfoolery"),
+    )
+
+
 class PriorCrimesForm(forms.Form):
-    PRIOR_CRIMES = (
-        ("BL", "Blarney"),
-        ("SK", "Skullduggery"),
-        ("TF", "Tomfoolery"),
+    prior_crimes = forms.MultipleChoiceField(
+        choices=PRIOR_CRIMES,
+        required=False,
+        widget=forms.SelectMultiple
         )
-    prior_crimes = forms.ChoiceField(choices=PRIOR_CRIMES)
 
 
 class Prop47CrimesForm(forms.Form):
-    PRIOR_CRIMES = (
-        ("HD", "Haberdashery"),
-        ("ST", "Sartorialisation"),
-        ("PH", "Poor Hygene"),
+    favorite_colors = forms.MultipleChoiceField(
+        required=False,
+        widget=forms.SelectMultiple,
+        choices=PROP_47_CRIMES
         )
-    prop47_crimes = forms.ChoiceField(choices=PRIOR_CRIMES)
 
 
 class FaxFormsForm(forms.Form):
