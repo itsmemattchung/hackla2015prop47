5be354fd29fa6f2f95e651dd7d706e9bde4aabe0
Matt Davis <matt@scarecrowvfx.com>
2015-06-07 09:40:29 -0700

    added data endpoints for courts, may have to verify them against data in csv's

--- prop47/urls.py
+++ prop47/urls.py
@@ -23,6 +23,13 @@ urlpatterns = [
     url(r'^faq/', TemplateView.as_view(template_name="faq.html")),
     url(r'^legal/', TemplateView.as_view(template_name="legal.html")),
     url(r'^step/(?P<step>[\w-]+)$', views.get_step_view, name="legal-step"),
-    url(r'^resources/', TemplateView.as_view(template_name="resources.html")),
-    url(r'^', TemplateView.as_view(template_name="index.html")),
+    url(r'^resources/$', TemplateView.as_view(template_name="resources.html")),
+    url(r'^step3/', TemplateView.as_view(template_name="enterzip.html")),
+    url(r'^locations/$', TemplateView.as_view(template_name="location_services.html")),
+    url(r'^locations/livescans/$', views.get_live_scan_results, name="live-scan-resources"),
+    url(r'^locations/courthouses/$', views.get_court_results, name="court-resources"),
+
+    url(r'^$', TemplateView.as_view(template_name="index.html")),
+
+
 ]
--- prop47/views.py
+++ prop47/views.py
@@ -1,8 +1,12 @@
-from django.views.generic.base import TemplateView
+from django.conf import settings
+from django.http import JsonResponse
 from django.shortcuts import render
+
+from googleplaces import GooglePlaces
 
 from .forms import PriorCrimesForm
 from .forms import Prop47CrimesForm
+
 
 def get_step_view(request, step):
     if int(step) == 1:
@@ -12,5 +16,55 @@ def get_step_view(request, step):
     return render(
         request,
         template_name="step.html",
-        context={"step":step, "form":pform},
+        context={"step": step, "form": pform},
         )
+
+
+def create_email_reminder(request):
+    return JsonResponse({"message": "Your reminder has been created!", "success": True})
+
+
+def print_result_instructions(request):
+    return JsonResponse({"message": "Here are your instructions!", "success": True})
+
+
+def get_court_results(request):
+    google_places = GooglePlaces(settings.GOOGLE_MAPS_API_KEY)
+    results_count = request.GET.get('count', 5)
+    place_results = google_places.nearby_search(
+        location=request.GET.get('zip_code'),
+        keyword="Courthouse",
+        radius=20000,
+        types=[]
+        )
+    map(lambda x: x.get_details(), place_results.places[:results_count])
+    json_results = [{
+        "name": tpr.name,
+        "address": tpr.formatted_address,
+        "website": tpr.website,
+        "phone_number": tpr.local_phone_number,
+        "url": tpr.url
+        }
+        for tpr in place_results.places[:results_count]]
+    return JsonResponse({"courthouses": json_results})
+
+
+def get_live_scan_results(request):
+    google_places = GooglePlaces(settings.GOOGLE_MAPS_API_KEY)
+    results_count = request.GET.get('count', 5)
+    place_results = google_places.nearby_search(
+        location=request.GET.get('zip_code'),
+        keyword="Live Scan",
+        radius=20000,
+        types=[]
+        )
+    map(lambda x: x.get_details(), place_results.places[:results_count])
+    json_results = [{
+        "name": tpr.name,
+        "address": tpr.formatted_address,
+        "website": tpr.website,
+        "phone_number": tpr.local_phone_number,
+        "url": tpr.url
+        }
+        for tpr in place_results.places[:results_count]]
+    return JsonResponse({"live_scans": json_results})
