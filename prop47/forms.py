88254bc0da361ebcdbf278d97eff2f008ca57813
matthewchung <itsmemattchung@gmail.com>
2015-06-06 21:10:21 -0700

    Cleaned up stale forms

--- prop47/forms.py
+++ prop47/forms.py
@@ -1,36 +1,6 @@
 from django import forms
 from localflavor.us.forms import USZipCodeField
 
-
-PROP_47_CRIMES = (
-    ("HD", "Haberdashery"),
-    ("ST", "Sartorialisation"),
-    ("PH", "Poor Hygene"),
-    )
-
-PRIOR_CRIMES = (
-    ("BL", "Blarney"),
-    ("SK", "Skullduggery"),
-    ("TF", "Tomfoolery"),
-    )
-
-
-class PriorCrimesForm(forms.Form):
-    prior_crimes = forms.MultipleChoiceField(
-        choices=PRIOR_CRIMES,
-        required=False,
-        widget=forms.SelectMultiple
-        )
-
-
-class Prop47CrimesForm(forms.Form):
-    favorite_colors = forms.MultipleChoiceField(
-        required=False,
-        widget=forms.SelectMultiple,
-        choices=PROP_47_CRIMES
-        )
-
-
 class FaxFormsForm(forms.Form):
     pass
 
