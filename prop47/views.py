8225fc09106caa4127da8ecd5121596833003249
Matt Davis <matt@scarecrowvfx.com>
2015-06-07 09:44:21 -0700

    adding new views to make merging easier

--- prop47/views.py
+++ prop47/views.py
@@ -4,21 +4,34 @@ from django.shortcuts import render
 
 from googleplaces import GooglePlaces
 
-from .forms import PriorCrimesForm
-from .forms import Prop47CrimesForm
-
 
 def get_step_view(request, step):
-    if int(step) == 1:
-        pform = Prop47CrimesForm()
-    else:
-        pform = PriorCrimesForm()
+    form = ResourcesSearchForm(request.GET or None)
     return render(
         request,
         template_name="step.html",
-        context={"step": step, "form": pform},
-        )
+        context={"step": step, "form": form}
+    )
 
+
+class ResourcesSearchView(TemplateView):
+    template_name = "resources.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(ResourcesSearchView, self).get_context_data(**kwargs)
+        form = ResourcesSearchForm(self.request.GET or None)
+        context['form'] = form
+        if form.is_valid():
+            zip_code = form.cleaned_data['zip_code']
+            url = "https://api.avvo.com/api/1/lawyers/search.json"
+            username = os.environ.get('AVVOS_USERNAME')
+            password = os.environ.get('AVVOS_APIKEY')
+            r = requests.get(url, auth=(username, password), params={
+                'loc': zip_code,
+                'q': 'Criminal Defense',
+            })
+            context['search_results'] = r.json()
+        return context
 
 def create_email_reminder(request):
     return JsonResponse({"message": "Your reminder has been created!", "success": True})
